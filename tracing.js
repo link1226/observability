51356ab27e763267a5708b65728a800d13daced8
link1226 <lin.k@ufl.edu>
2024-12-01 19:07:12 -0500

    code

---
+++ tracing.js
@@ -0,0 +1,34 @@
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { trace } = require("@opentelemetry/api");
+
+module.exports = (serviceName) => {
+  const exporter = new JaegerExporter({
+    endpoint: "http://localhost:14268/api/traces",
+  });
+
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+  provider.register();
+
+  registerInstrumentations({
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+    ],
+    tracerProvider: provider,
+  });
+
+  return trace.getTracer(serviceName);
+};
